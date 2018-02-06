4ca3717ef098a95cf02d99be4d990667be613179
WHJ <760503653@qq.com>
2018-02-06 15:49:22 +0800

    Initial Commit

---
+++ HJSpecialEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HJSpecialEffect
+//
+//  Created by WHJ on 2018/2/6.
+//  Copyright © 2018年 WHJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
